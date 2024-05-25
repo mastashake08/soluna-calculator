e4e3be419524237fc76e440f8132e1abf935e9c5
Hacker God <jyrone.parker@gmail.com>
2024-05-25 05:07:32 -0400

    notification

--- app.vue
+++ app.vue
@@ -7,8 +7,9 @@
           <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
         </div>
       </template>
-      <img ref="isShowing" v-if="isShowing" src="~/assets/svg/full-moon.svg" />
+      <!-- <img ref="isShowing" v-if="isShowing" src="~/assets/svg/full-moon.svg" /> -->
       <UButton class="mx-auto" @click="getData">Get Lunar Data</UButton>
+      {{ moon }}
     </UCard>
   </UContainer>
 </template>
@@ -17,11 +18,15 @@
 import SunCalc from 'suncalc'
 
 const isShowing = ref('isShowing', false)
+const moon = useState('moon', () => {})
 function getData() {
   if (!navigator.geolocation) {
     alert("Geolocation is not supported by your browser")
   } else {
     navigator.geolocation.getCurrentPosition(getTimes, error);
+    
+
+    
   }
 }
 function getTimes(position) {
@@ -29,6 +34,14 @@ function getTimes(position) {
   var times = SunCalc.getTimes(Date.now(), position.coords.latitude, position.coords.longitude);
   console.log(times)
   isShowing.value = !isShowing.value
+  moon.value = SunCalc.getMoonPosition(Date.now(), position.coords.latitude, position.coords.longitude)
+  const ill = SunCalc.getMoonIllumination(Date.now())
+  Notification.requestPermission().then((result) => {
+    const img = "/full-moon.svg";
+    const text = `The moon is ${moon.value.altitude} radian above the horizons and ${moon.value.distance} km away.`;
+    const notification = new Notification(`${ill.fraction}% illuminated moon today`, { body: text, icon: img });
+    console.log(notification)
+  });
 }
 
 function error(e) {
